e86aafd3f98214629fddb6ec0af2b6951a0c7b36
bjorn <bjorn@bjornkjellgren.se>
2022-09-26 13:26:24 +0200

    automattester för uppgift 22

---
+++ vecka3/test_uppgift_22.py
@@ -0,0 +1,24 @@
+import pytest
+import uppgift_22
+
+
+
+
+def test_square():
+    assert uppgift_22.square(4) == 16
+
+
+def test_circumference():
+    assert uppgift_22.circumference(2) == pytest.approx(12.5664, 0.001)
+
+
+def test_radius():
+    assert uppgift_22.radius(12.5664) == pytest.approx(2, 0.001)
+
+
+def test_factorial():
+    assert uppgift_22.factorial(5) == 120
+
+def test_factorial_0():
+    assert uppgift_22.factorial(0) == 1
+
