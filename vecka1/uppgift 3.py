e2fa91662c1d8287b7d8fa1133c7be1c9de2e3ec
bjorn <bjorn@bjornkjellgren.se>
2022-11-07 11:13:49 +0100

    Fixade kanske uppgift 3

--- vecka1/uppgift 3.py
+++ vecka1/uppgift 3.py
@@ -4,17 +4,17 @@
 import random
 
 n = random.randint(1, 20)
-print("I'm thinking of a number between 1 and 20. Guess which?")
+print("Jag tänker på ett tal mellan 1 och 20")
 while True:
-    text = input("Your guess: ")
+    text = input("Din gissning: ")
     as_number = int(text)
 
     if as_number == n:
-        print("Correct!")
+        print("Rätt!")
         break
 
     if as_number < n:
-        print("Wrong, my number is higher... Try again!")
+        print("Fel. Talet är högre, försök igen!")
 
     if as_number > n:
-        print("Wrong, my number is lower... Try again!")
+        print("Fel. Talet är lägre, försök igen!")
