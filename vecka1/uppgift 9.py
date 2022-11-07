45e43eab995f3d48c328fef567f98126eb633bc5
bjorn <bjorn@bjornkjellgren.se>
2022-11-07 13:38:00 +0100

    Fixade uppgift 9

--- vecka1/uppgift 9.py
+++ vecka1/uppgift 9.py
@@ -7,23 +7,23 @@
 import random
 
 def game():
-correct_answers = 0
-for i in range(3):
-a = random.randint(1, 10)
-b = random.randint(1, 10)
-answer = input(str(a) + "+" + str(b) + "=")
-number = int(answer)
+    correct_answers = 0
+    for i in range(3):
+        a = random.randint(1, 10)
+        b = random.randint(1, 10)
+        answer = input(str(a) + "+" + str(b) + "=")
+        number = int(answer)
 
-if number == a + b:
-print("Rätt!")
-correct_answers += 1
-else:
-print(f"Fel... Det blir {a+b}")
-print("---")
-print("Du fick " + str(correct_answers) + " av 3 rätt.")
+        if number == a + b:
+            print("Rätt!")
+            correct_answers += 1
+        else:
+            print(f"Fel... Det blir {a+b}")
+        print("---")
+        print("Du fick " + str(correct_answers) + " av 3 rätt.")
 
 if __name__ == '__main__':
-game()
+    game()
 
 
 
